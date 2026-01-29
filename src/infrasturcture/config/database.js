9886cc908183fbf5c1f3ced3298c98230b20a92c
Beli Dermawan <belidermawan01@gmail.com>
2026-01-29 14:46:29 +0700

    modif

--- src/infrasturcture/config/database.js
+++ src/infrasturcture/config/database.js
@@ -1,4 +1,13 @@
 const dotenv = require("dotenv")
+
+module.exports = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  logging: false
+}
 module.exports = {
   "development": {
     "username": "root",
