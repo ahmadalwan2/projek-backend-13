b53ab275892287b12b9cc82f0551539fa927de61
hamdani <hamdankatsiron4@gmail.com>
2026-01-29 12:52:27 +0700

    buat koneksinya

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const sequelize = require("./infrasturcture/config/koneksi.js");
+
+
+const path = require("path")
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", async(req, res) => {
+    try {
+        await sequelize.authenticate();
+        return res.json({message:"berhasil"})
+    } catch (error) {
+        return res.json({message:error.message})
+    }
+})
+app.use("/api", express.static(path.join(__dirname, "uploads")));
+
+
+// app.use("/api/alat", routerAalat);
+// app.use("/api/peminjaman", routerPeminjaman)
+app.listen(PORT, () => {
+  console.log(`Server jalan di port ${PORT}`);
+});
+
