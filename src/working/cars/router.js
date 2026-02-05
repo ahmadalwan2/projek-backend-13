e7691eec3dd7acbaa222f00b2b5a8c7f27052ad7
ahmadalwan <madajadeh2023@gmail.com>
2026-02-05 13:16:13 +0700

    commit file ini

--- src/working/cars/router.js
+++ src/working/cars/router.js
@@ -1,6 +1,6 @@
 const express = require ("express");
 const { getAllCars, getById, deleteCars, createCars, updateCars } = require ("./controller.js");
-const {cekId, upload, cekRegister, validateParams} = require ("./middleware.js");
+const {cekId, cekRegister, uploadMiddleware} = require ("./middleware.js");
 const {  
   auth,
   isAdmin} = require("../../middlewares/middle.js");
@@ -9,8 +9,8 @@ const router = express.Router();
 router.get("/",auth, isAdmin, getAllCars);
 router.get("/cars/:id",auth, isAdmin, getById);
 router.delete("/delete/:id",auth, isAdmin, cekId, deleteCars);
-router.post("/create", upload.single("foto_mobil"),cekRegister, createCars)
-router.patch("/cars/:id", upload.single("foto_mobil"),auth, isAdmin, cekId, cekRegister, updateCars);
+router.post("/create", uploadMiddleware, cekRegister, createCars)
+router.patch("/cars/:id", auth, isAdmin, cekId, uploadMiddleware, cekRegister, updateCars);
 
 
 module.exports = router;
